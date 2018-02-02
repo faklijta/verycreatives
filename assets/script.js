72bc8aa927c6917d5aeed61b9c610ff01d72b0df
faklijta <faklijta@gmail.com>
2018-02-02 13:17:14 +0100

    refact(script and style moved to assets folder)

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function ajax (method, url, data, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        var requestAPI = JSON.parse(xhr.response);
+        callback(requestAPI);
+      }
+    });
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
+};
+
+function drawBall() {
+  var display = document.querySelector('display')
+  var ball = document.createElement("figure").classList.add('ball')
+  display.appendChild(ball)
+};
+
+var button = document.querySelector('button');
+button.addEventListener('click', function () {
+  var url = 'http://localhost:3000/';
+  ajax('GET', url, null, drawBall)
+});
+
